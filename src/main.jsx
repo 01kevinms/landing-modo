8cbfa6b033850e028611653c21765815a4d01d3e
01kevinms <kevin.victors29@gmail.com>
2025-11-17 23:22:31 -0300

    corrects

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './routes/Routes.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/landing-modo">
+    <HashRouter basename="/landing-modo">
     <Router />    
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
